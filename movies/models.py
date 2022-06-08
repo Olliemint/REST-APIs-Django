955332427a3f290d7a7e48c35526c76af25cc4e7
Olliemint <furywrld5@gmail.com>
2022-06-09 02:07:00 +0300

    feat: class based view for api

---
+++ movies/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Moviedata(models.Model):
+    
+    name = models.CharField(max_length=200)
+    duration = models.FloatField()
+    ratings = models.FloatField()
+    
+    def __str__(self):
+        
+        return self.name
+        
+        
